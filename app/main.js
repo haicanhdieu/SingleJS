a244133b76448ab33fba35a0b36c5f3040493251
NTML <ntm.xyz@gmail.com>
2015-08-19 23:50:13 +0700

    Create repo

---
+++ app/main.js
@@ -0,0 +1,36 @@
+﻿requirejs.config({
+    paths: {
+        'singlejs': '../vendor/singlejs/js',
+        'angular': '../vendor/angular/angular.min',
+        'materialize': '../vendor/materialize/js/materialize.min',
+        'jquery': '../vendor/jquery/jquery-2.1.4.min',
+      },
+      shim: {
+          'materialize': {
+              deps: ['jquery'],
+              exports: 'jQuery'
+         },
+      }
+});
+
+define(['singlejs/system', 'singlejs/app'],  function (system, app) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Book & Buzz';
+
+    app.configurePlugins({
+        router:true,
+        dialog: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
